8c68575c5df8b941ec76d2dd6ca8fd16e05d4a8c
MD UMAR HUSSAIN <115338236+M-hussai@users.noreply.github.com>
2026-08-08 15:42:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+
+let box1=document.getElementById("square1")
+let box2=document.getElementById("square2")
+let box3=document.getElementById("square3")
+box1.addEventListener("mouseover",()=>{
+    box2.style.backgroundColor="#6F4E37"
+    box3.style.backgroundColor="#6F4E37"
+
+})
+box1.addEventListener("mouseleave",()=>{
+    box2.style.backgroundColor="#E6E6FA"
+    box3.style.backgroundColor="#E6E6FA"
+
+})
+
+box2.addEventListener("mouseover",()=>{
+    box1.style.backgroundColor="#6F4E37"
+    box3.style.backgroundColor="#6F4E37"
+
+})
+box2.addEventListener("mouseleave",()=>{
+    box1.style.backgroundColor="#E6E6FA"
+    box3.style.backgroundColor="#E6E6FA"
+
+})
+
+
+box3.addEventListener("mouseover",()=>{
+    box1.style.backgroundColor="#6F4E37"
+    box2.style.backgroundColor="#6F4E37"
+
+})
+box3.addEventListener("mouseleave",()=>{
+    box1.style.backgroundColor="#E6E6FA"
+    box2.style.backgroundColor="#E6E6FA"
+
+})
